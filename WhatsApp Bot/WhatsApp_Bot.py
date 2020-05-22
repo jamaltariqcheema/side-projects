61005031d69dcdd7406c77c5dd226aeca82a7286
Jamal Tariq Cheema <jamaltariqcheema@gmail.com>
2020-05-22 15:57:25 +0500

    Added support for Safari

--- WhatsApp Bot/WhatsApp_Bot.py
+++ WhatsApp Bot/WhatsApp_Bot.py
@@ -1,12 +1,34 @@
 from selenium import webdriver
 import time
+
+# CHROME BOT
 chrome_browser=webdriver.Chrome(executable_path='/Users/jamaltariqcheema/Downloads/chromedriver')
 chrome_browser.get('https://web.whatsapp.com/')
+print('Waiting...')
 time.sleep(15)
-print('Waiting for scan')
-user=chrome_browser.find_element_by_xpath('//span[@title="Test"]')
-user.click()
-Text=chrome_browser.find_element_by_xpath('//div[@class="_1Plpp"]')
-Text.send_keys('Bot checking...')
-send=chrome_browser.find_element_by_xpath('//button[@class="_35EW6"]')
-send.click()
+try:
+    chrome_user=chrome_browser.find_element_by_xpath('//span[@title="Test"]')
+    chrome_user.click()
+    chrome_text=chrome_browser.find_element_by_xpath('//div[@class="_1Plpp"]')
+    chrome_text.send_keys('Bot checking...')
+    chrome_send=chrome_browser.find_element_by_xpath('//button[@class="_35EW6"]')
+    chrome_send.click()
+except:
+    print('Run, Aliens Invaded the Browser')
+
+# SAFARI BOT
+safari=webdriver.Safari(executable_path='/usr/bin/safaridriver')
+safari.get('https://web.whatsapp.com')
+print('Waiting...')
+time.sleep(15)
+try:
+    safari_user = safari.find_element_by_xpath('//span[@title="Test"]')
+    safari_user.click()
+    for i in range(0, 10):
+        safari_text = safari.find_element_by_xpath('//div[@class="_1Plpp"]')
+        safari_text.send_keys('SPAMMING After Every 5 Seconds')
+        safari_send = safari.find_element_by_xpath('//button[@class="_35EW6"]')
+        safari_send.click()
+        time.sleep(5)
+except:
+    print('Run, Aliens Invaded the Browser')
