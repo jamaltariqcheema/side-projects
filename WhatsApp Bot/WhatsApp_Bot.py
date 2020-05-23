2df4c2bc0b2fe017c58a74f4a6a13da3f27a008b
Jamal Tariq Cheema <jamaltariqcheema@gmail.com>
2020-05-23 19:46:59 +0500

    Added support for multiple users in chrome

--- WhatsApp Bot/WhatsApp_Bot.py
+++ WhatsApp Bot/WhatsApp_Bot.py
@@ -7,12 +7,14 @@ chrome_browser.get('https://web.whatsapp.com/')
 print('Waiting...')
 time.sleep(15)
 try:
-    chrome_user=chrome_browser.find_element_by_xpath('//span[@title="Test"]')
-    chrome_user.click()
-    chrome_text=chrome_browser.find_element_by_xpath('//div[@class="_1Plpp"]')
-    chrome_text.send_keys('Bot checking...')
-    chrome_send=chrome_browser.find_element_by_xpath('//button[@class="_35EW6"]')
-    chrome_send.click()
+    ch_users=['Test1','Test2']
+    for ch_user in ch_users:
+        chrome_user=chrome_browser.find_element_by_xpath('//span[@title="{}"]'.format(ch_user))
+        chrome_user.click()
+        chrome_text=chrome_browser.find_element_by_xpath('//div[@class="_1Plpp"]')
+        chrome_text.send_keys('Bot checking...')
+        chrome_send=chrome_browser.find_element_by_xpath('//button[@class="_35EW6"]')
+        chrome_send.click()
 except:
     print('Run, Aliens Invaded the Browser')
 
